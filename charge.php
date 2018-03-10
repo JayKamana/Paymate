fe03b2500db4b8ddad2aff80d8017751a69f206f
James Kamana <jaykamana@gmail.com>
2018-03-10 13:56:13 +0300

    Add Customer and Transaction models

--- charge.php
+++ charge.php
@@ -1,5 +1,9 @@
 <?php
   require_once('vendor/autoload.php');
+  require_once('config/db.php');
+  require_once('lib/pdo_db.php');
+  require_once('models/Customer.php');
+  require_once('models/Transaction.php');
 
   \Stripe\Stripe::setApiKey('sk_test_MGb2HIP5jSbPyYI787cQct37');
 
@@ -21,6 +25,28 @@
     "description" => "Intro To React Course",
     "customer" => $customer->id
   ));
+
+  $customerData = [
+    'id' => $charge->customer,
+    'first_name' => $first_name,
+    'last_name' => $last_name,
+    'email' => $email
+  ];
+
+  $customer = new Customer();
+  $customer->addCustomer($customerData);
+
+  $transactionData = [
+    'id' => $charge->id,
+    'customer_id' => $charge->customer,
+    'product' => $charge->description,
+    'amount' => $charge->amount,
+    'currency' => $charge->currency,
+    'status' => $charge->status
+  ];
+
+  $transaction = new Transaction();
+  $transaction->addTransaction($transactionData);
 
   header('Location: success.php?tid='.$charge->id.'&product='.$charge->description);
 
